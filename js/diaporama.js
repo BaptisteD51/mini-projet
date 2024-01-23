46e4341c8c37b78ac87b604fac0c133fbb019bb2
Baptiste Dufour <dufourbaptiste08@gmail.com>
2024-01-23 13:58:37 +0100

    Responsive slider

--- js/diaporama.js
+++ js/diaporama.js
@@ -1,35 +1,45 @@
-var carrousel = document.querySelector("div.carrousel")
-var slides = document.querySelectorAll("div.slide")
+var sliding
 
-mainWidth = document.querySelector("main").offsetWidth
-carrousel.style.width = mainWidth*slides.length + "px"
-carrousel.style.left = -mainWidth + "px"
+function slider(){
+    var carrousel = document.querySelector("div.carrousel")
+    var slides = document.querySelectorAll("div.slide")
 
-slides.forEach(function(slide){
-    slide.style.width = mainWidth + "px"
-})
+    mainWidth = document.querySelector("main").offsetWidth
+    carrousel.style.width = mainWidth*slides.length + "px"
+    carrousel.style.left = -mainWidth + "px"
 
-function slideIn(elmt){
-    var position = parseInt(elmt.dataset.position)
-    if( position==2 ){
-        position = 0
-        elmt.dataset.position = position
-        elmt.style.zIndex = "0"
-    }else{
-        position++
-        elmt.dataset.position = position
-        elmt.style.zIndex = "1"
+    slides.forEach(function(slide){
+        slide.style.width = mainWidth + "px"
+    })
+
+    function slideIn(elmt){
+        var position = parseInt(elmt.dataset.position)
+        if( position==2 ){
+            position = 0
+            elmt.dataset.position = position
+            elmt.style.zIndex = "0"
+        }else{
+            position++
+            elmt.dataset.position = position
+            elmt.style.zIndex = "1"
+        }
+        elmt.style.left = (position * mainWidth) + "px" 
     }
-    elmt.style.left = (position * mainWidth) + "px" 
+
+    slides.forEach(function(slide){
+        slideIn(slide)
+    })
+
+    sliding = setInterval(function(){
+        slides.forEach(function(slide){
+            slideIn(slide);
+        })
+    },4000)
 }
 
-slides.forEach(function(slide){
-    slideIn(slide)
+slider()
+
+window.addEventListener("resize", function(){
+    clearInterval(sliding)
+    slider()
 })
-
-
-setInterval(function(){
-    slides.forEach(function(slide){
-        slideIn(slide);
-    })
-},4000)
